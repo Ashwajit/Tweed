f0068c21fdb450be2dcd9f36009d21d2f69df314
Andrew Slavin <andrew.slavin@gmail.com>
2019-03-20 09:24:12 +0000

    Got 1 test running using base page object and base test.

---
+++ Tweed/Tests/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tweed.Tests
+{
+    public class BaseTest
+    {
+
+        protected IWebDriver driver;
+        public string homeURL;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            homeURL = "https://uk.virginmoney.com";
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(homeURL);
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            driver.Close();
+        }
+    }
+}
--- Tweed/Tests/Functional/GlobalNavTest.cs
+++ Tweed/Tests/Functional/GlobalNavTest.cs
@@ -4,38 +4,21 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using Tweed.PageObjects;
 
-namespace Tweed
+namespace Tweed.Tests
 {
     [TestFixture]
-    public class GlobalNavTest
+    public class GlobalNavTest : BaseTest
     {
-        private IWebDriver driver;
-        public string homeURL;
 
         [Test(Description = "Check Global Nav contains the company logo")]
         public void GlobalNavContainsCorrectElements()
         {
-            driver.Navigate().GoToUrl(homeURL);
             GlobalNavComponent globalNavComponent = new GlobalNavComponent(driver);
 
             Assert.AreEqual(1, 2);
         }
 
 
-        [TearDown]
-        public void TearDownTest()
-        {
-            driver.Close();
-        }
-
-
-        [SetUp]
-        public void SetupTest()
-        {
-            homeURL = "https://uk.virginmoney.com";
-            driver = new ChromeDriver();
-
-        }
         
     }
 }
